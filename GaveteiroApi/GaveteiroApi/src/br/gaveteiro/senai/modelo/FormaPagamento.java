c52de0fe9a3950feb1716e0d05b450f315224b1f
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2016-09-18 15:08:25 -0300

    Classes Models

---
+++ GaveteiroApi/GaveteiroApi/src/br/gaveteiro/senai/modelo/FormaPagamento.java
@@ -0,0 +1,18 @@
+package br.gaveteiro.senai.modelo;
+
+public class FormaPagamento {
+	private Long idFormaPagamento;
+	private String descricao;
+	public Long getIdFormaPagamento() {
+		return idFormaPagamento;
+	}
+	public void setIdFormaPagamento(Long idFormaPagamento) {
+		this.idFormaPagamento = idFormaPagamento;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	} 
+}
