95c03ff4061ef75db5249378f265c4ef916a0a27
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2016-09-18 14:39:57 -0300

    Criação de model Produto

---
+++ GaveteiroApi/GaveteiroApi/src/br/gaveteiro/senai/modelo/Empresa.java
@@ -0,0 +1,41 @@
+package br.gaveteiro.senai.modelo;
+
+public class Empresa {
+	private Long idEmpresa;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String razaoSocial;
+	
+	public Long getIdEmpresa() {
+		return idEmpresa;
+	}
+	public void setIdEmpresa(Long idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+}
--- GaveteiroApi/GaveteiroApi/src/br/gaveteiro/senai/modelo/Produto.java
+++ GaveteiroApi/GaveteiroApi/src/br/gaveteiro/senai/modelo/Produto.java
@@ -1,5 +1,34 @@
 package br.gaveteiro.senai.modelo;
 
 public class Produto {
+	private Long idProduto;
+	private Double precoUnitario;
+	private String descricao;
+	private String categoria;
+	public Long getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	public Double getPrecoUnitario() {
+		return precoUnitario;
+	}
+	public void setPrecoUnitario(Double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
 
 }
